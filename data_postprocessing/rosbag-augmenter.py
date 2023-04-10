04b11824214c471b7a902a58828d960441a7db4f
Ben Zhang <ben.zhang@watonomous.ca>
2023-04-10 19:19:21 +0000

    Make rosbag-augmenter take in CLI arguments

--- data_postprocessing/rosbag-augmenter.py
+++ data_postprocessing/rosbag-augmenter.py
@@ -7,51 +7,66 @@ from pathlib import Path
 from rosbags.typesys.types import geometry_msgs__msg__Vector3 as Vector3
 import transformations as tf
 from tqdm import tqdm
+import argparse
 
 S_TO_NS = 1e9
-
-EXPERIMENTS_PATH = Path('./experiments').expanduser()
-ROSBAG_PATH = EXPERIMENTS_PATH / '2023-04-10T17:54:09+00:00_myexp/2023-04-10T17:54:09+00:00-myexp.bag'
-AUGMENTED_ROSBAG_PATH = ROSBAG_PATH.with_suffix('.augmented.bag')
-
-# delete augmented bag if it exists
-AUGMENTED_ROSBAG_PATH.unlink(missing_ok=True)
 
 def orientation_to_rpy_vec3(orientation) -> Vector3:
     quaternion = (orientation.x, orientation.y, orientation.z, orientation.w)
     roll, pitch, yaw = tf.euler_from_quaternion(quaternion, axes='xyzs')
     return Vector3(x=roll, y=pitch, z=yaw)
 
-# create reader instance
-with Reader([ROSBAG_PATH]) as reader, Writer(AUGMENTED_ROSBAG_PATH) as writer:
-    start_time_s = reader.start_time / S_TO_NS
-    end_time_s = reader.end_time / S_TO_NS
-    duration_s = end_time_s - start_time_s
-    print(f"Start time (s): {start_time_s}, end time (s): {end_time_s}, duration (s): {duration_s}")
+def main(rosbag_path: Path, output_path: Path):
+    # delete augmented bag if it exists
+    output_path.unlink(missing_ok=True)
 
-    # copy connections from reader to writer
-    writer.connections = reader.connections
+    # create reader instance
+    with Reader([rosbag_path]) as reader, Writer(output_path) as writer:
+        start_time_s = reader.start_time / S_TO_NS
+        end_time_s = reader.end_time / S_TO_NS
+        duration_s = end_time_s - start_time_s
+        print(f"Start time (s): {start_time_s}, end time (s): {end_time_s}, duration (s): {duration_s}")
 
-    odometry_connections = [x for x in reader.connections if x.msgtype == 'nav_msgs/msg/Odometry']
-    quaternion_to_rpy_connection_mapping = {}
+        # copy connections from reader to writer
+        writer.connections = reader.connections
 
-    for connection in odometry_connections:
-        topic = connection.topic + '/orientation_rpy'
-        msgtype = Vector3.__msgtype__
-        new_connection = writer.add_connection(topic, msgtype)
-        quaternion_to_rpy_connection_mapping[connection.topic] = (new_connection, msgtype)
+        odometry_connections = [x for x in reader.connections if x.msgtype == 'nav_msgs/msg/Odometry']
+        quaternion_to_rpy_connection_mapping = {}
 
-    with tqdm(total=round(duration_s,4), desc="Processing rosbag", unit="s") as pbar:
-        # iterate over messages
-        for connection, timestamp, rawdata in reader.messages():
-            pbar.update(round(timestamp / S_TO_NS - pbar.n - start_time_s, 4))
+        for connection in odometry_connections:
+            topic = connection.topic + '/orientation_rpy'
+            msgtype = Vector3.__msgtype__
+            new_connection = writer.add_connection(topic, msgtype)
+            quaternion_to_rpy_connection_mapping[connection.topic] = (new_connection, msgtype)
 
-            if quaternion_to_rpy_connection_mapping.get(connection.topic):
-                out_connection, out_msgtype = quaternion_to_rpy_connection_mapping[connection.topic]
-                msg = deserialize_cdr(ros1_to_cdr(rawdata, connection.msgtype), connection.msgtype)
-                vec3 = orientation_to_rpy_vec3(msg.pose.pose.orientation)
-                writer.write(out_connection, timestamp, cdr_to_ros1(serialize_cdr(vec3, out_msgtype), out_msgtype))
-            else:
-                pass
+        with tqdm(total=round(duration_s,4), desc="Processing rosbag", unit="s") as pbar:
+            # iterate over messages
+            for connection, timestamp, rawdata in reader.messages():
+                pbar.update(round(timestamp / S_TO_NS - pbar.n - start_time_s, 4))
 
-            writer.write(connection, timestamp, rawdata)
+                if quaternion_to_rpy_connection_mapping.get(connection.topic):
+                    out_connection, out_msgtype = quaternion_to_rpy_connection_mapping[connection.topic]
+                    msg = deserialize_cdr(ros1_to_cdr(rawdata, connection.msgtype), connection.msgtype)
+                    vec3 = orientation_to_rpy_vec3(msg.pose.pose.orientation)
+                    writer.write(out_connection, timestamp, cdr_to_ros1(serialize_cdr(vec3, out_msgtype), out_msgtype))
+                else:
+                    pass
+
+                writer.write(connection, timestamp, rawdata)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('rosbag_path', type=str, help='Path to the rosbag to augment')
+    parser.add_argument('--output_path', type=str, required=False, help='Path to the augmented rosbag (default: <rosbag_path>.augmented.bag)')
+    args = parser.parse_args()
+
+    rosbag_path = Path(args.rosbag_path)
+    if not rosbag_path.exists():
+        raise FileNotFoundError(f"Rosbag path {rosbag_path} does not exist")
+    
+    if args.output_path:
+        output_path = Path(args.output_path)
+    else:
+        output_path = rosbag_path.with_suffix('.augmented.bag')
+
+    main(rosbag_path, output_path)
